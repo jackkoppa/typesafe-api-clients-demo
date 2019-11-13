975c0f04427aac3dc87492bed8e7445b32e8d37e
Jack Koppa <hello@jackkoppa.com>
2019-11-13 18:32:03 -0500

    ci: Log when starting to copy CNAME

--- build/copyCname.js
+++ build/copyCname.js
@@ -1,6 +1,7 @@
 const { copyFileSync } = require("fs");
 
 try {
+  console.log("copying CNAME");
   copyFileSync("./build/CNAME", "./dist/CNAME");
 } catch (error) {
   console.warn("failed to copy CNAME", error);
