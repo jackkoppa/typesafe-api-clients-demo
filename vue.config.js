35e3e29bd2070866f23020cfd62d5ee4ecce10cc
Jack Koppa <hello@jackkoppa.com>
2019-11-13 13:16:50 -0500

    ci: Use public path when building from Travis

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,9 @@ module.exports = {
   devServer: {
     port: 8080,
     host: "localhost.jackkoppa.com"
-  }
+  },
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "https://jackkoppa.github.io/typesafe-api-clients-demo/"
+      : "/"
 };
